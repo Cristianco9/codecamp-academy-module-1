2688f4ea1299c4a2ad066f086b9fe9966ea5cff3
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-03-18 09:57:12 -0500

    class 44 03-18-2026

--- class-44/index.js
+++ class-44/index.js
@@ -15,16 +15,68 @@ console.log("Value of the variable 'a': " + a); // 30
 console.log("Value of the variable 'b': " + b); // 30
 console.log("Value of the variable 'c': " + c); // 30
 
-console.log("c == a :");
+console.log("c == a :"); // 30 == 30
 console.log(c == a); // true
 a = "new value";
-console.log("Value of the variable 'a': " + a);
-console.log("c == a :");
+console.log("Value of the variable 'a': " + a); // "new value"
+console.log("c == a :"); // "new value" == 30
 console.log(c == a); // false
-console.log("b == c :");
+console.log("b == c :"); // 30 == 30
 console.log(b == c); // true
 
 // what is the 'c' variable value ?
 console.log("Value of the variable 'a': " + a); // "new value"
 console.log("Value of the variable 'b': " + b); // 30
 console.log("Value of the variable 'c': " + c); // 30
+/* ========================================================================== */
+console.log("====================================================");
+
+// What's the difference between '==' and '==='
+
+// same value, same data type
+console.log("value comparison");
+console.log("5 == 5 :");
+console.log(5 == 5);  // true
+console.log("value and data type comparison");
+console.log("5 === 5 :");
+console.log(5 === 5); // true
+
+
+// same value, different data type
+// number 5 == string '5'
+console.log("value comparison");
+console.log("5 == '5' :");
+console.log(5 == '5'); // true
+// number 5 === string '5'
+console.log("value and data type comparison");
+console.log("5 === '5' :"); 
+console.log(5 === '5'); // false\
+
+
+// Why?
+/* 
+    because
+        ==  : compare value
+        === : compare value and data type
+*/
+
+/* 
+        Boolean theory: 
+        LOGIC GATES :   STATE   
+            1       :   true
+            0       :   false
+*/
+console.log("value comparison");
+console.log(" 1 == true :");
+console.log(1 == true); // true
+console.log("value and data type comparison");
+console.log("1 === true :");
+console.log(1 === true); // false
+
+/* 
+    why is false?
+    1 = numeric and true = boolean
+    numeric === boolean => false
+*/
+
+
