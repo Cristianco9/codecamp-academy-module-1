3ea31f1c31d2d2ffd25962d859224629d766ee31
cristian-air-m2 <cristian_cortes_ortiz@hotmail.com>
2026-04-14 08:51:35 -0500

    class 63 04-14-2026

--- class-63/problem-29/problem-29.js
+++ class-63/problem-29/problem-29.js
@@ -1,6 +1,6 @@
 // Author: Cristian Camilo Cortes Ortiz
 
-// Variable to store the total points
+// Global variable to store the total points
 let totalPoints = 0;
 
 // Loop through the 5 leves
@@ -15,3 +15,48 @@ for (let level = 1; level <= 5; level++) {
 
 // Print the final result
 console.log("Total points earned: ", totalPoints);
+
+/*
+DESKTOP TEST (DRY RUN)
+
+Initial values:
+totalPoints = 0
+
+-------------------------------------------------
+Iteration 1
+level = 1
+points = level * 10 = 1 * 10 = 10
+totalPoints = totalPoints + points
+totalPoints = 0 + 10 = 10
+
+-------------------------------------------------
+Iteration 2
+level = 2
+points = 2 * 10 = 20
+totalPoints = 10 + 20 = 30
+
+-------------------------------------------------
+Iteration 3
+level = 3
+points = 3 * 10 = 30
+totalPoints = 30 + 30 = 60
+
+-------------------------------------------------
+Iteration 4
+level = 4
+points = 4 * 10 = 40
+totalPoints = 60 + 40 = 100
+
+-------------------------------------------------
+Iteration 5
+level = 5
+points = 5 * 10 = 50
+totalPoints = 100 + 50 = 150
+
+Loop ends because level > 5
+
+Final value:
+totalPoints = 150
+-------------------------------------------------
+
+*/
